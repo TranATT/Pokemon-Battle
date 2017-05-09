17ca14c7ef59d34797415f32ab6200c09d672369
JeremyLawton <Jeremy.Lawton@comcast.net>
2017-05-09 14:45:17 -0700

    Update PokemonDriver.java

--- Pokemon Battle Sos Edition Period 6/PokemonDriver.java
+++ Pokemon Battle Sos Edition Period 6/PokemonDriver.java
@@ -1,7 +1,13 @@
+import java.util.Random;
 public class PokemonDriver
 {
   public static void main(String args[])
   {
-    Battle battle = new Battle();
+    Battle battle = new Battle(new Pokemon pokemon1, new Pokemon pokemon2);
+    
+    Random randomizer = new Random();
+    //decide who goes first
+    int first = randomizer.nextInt(2);
+    
   }
 }
