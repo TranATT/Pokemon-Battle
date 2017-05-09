0945d7ed4958ddf5416e981b0561761767b24437
JeremyLawton <Jeremy.Lawton@comcast.net>
2017-05-09 14:27:55 -0700

    Update Pokemon.java

--- Pokemon Battle Sos Edition Period 6/Pokemon.java
+++ Pokemon Battle Sos Edition Period 6/Pokemon.java
@@ -2,31 +2,41 @@
 public class Pokemon implements PokemonInterface
 {
     private int HP;
-    private int attack1Damage;
-    private String attack1Name;
+    private ArrayList <Object> attacks;
+   /* private int attack1Damage;
+    private String atStrtack1Name;
     private int attack2Damage;
     private String attack2Name;
     private int attack3Damage;
     private String attack3Name;
     private int attack4Damage;
-    private String attack4Name;
+    private String attack4Name; */
     
     public Pokemon()
     {
-        HP = 99;
-        attack1Damage = 1;
+        attacks = new ArrayList <Object> ();
+         attacks.add("a1");
+        attacks.add(1);
+        attacks.add("a2");
+        attacks.add(2);
+        attacks.add("a3");
+        attacks.add(3);
+        attacks.add("a4");
+        attacks.add(4);
+       HP = 99;
+       /* attack1Damage = 1;
         attack2Damage = 2;
         attack3Damage = 3;
         attack4Damage = 4;
         attack1Name = "name";
         attack2Name = "a name";
         attack3Name = "the name";
-        attack4Name = "my name";
+        attack4Name = "my name"; */
         
     }
     
     public int attack () {
-    //buttons + if-return statement
+    //buttons + return attack number
     }
     
     public void setHP(int newHP)
