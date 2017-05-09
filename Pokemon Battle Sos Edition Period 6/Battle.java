9132b468793f0a317846a6a9f71f26fbd849fcfd
JeremyLawton <Jeremy.Lawton@comcast.net>
2017-05-09 13:56:36 -0700

    Update Battle.java

--- Pokemon Battle Sos Edition Period 6/Battle.java
+++ Pokemon Battle Sos Edition Period 6/Battle.java
@@ -1,4 +1,9 @@
 public class Battle {
+    private Pokemon player1;
+    private Pokemon player2;
+    
     public Battle(Pokemon one, Pokemon two) {
+        player1 = one;
+        player2 = two;
     }
 }
