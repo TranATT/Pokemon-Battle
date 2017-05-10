dc7e0587e5d6d45af94db22070bc771b7fdb01f1
TranATT <atran750@gmail.com>
2017-05-10 14:35:34 -0700

    Add files via upload
    GUIIIIII

---
+++ Pokemon Battle Sos Edition Period 6/GUI.java
@@ -0,0 +1,82 @@
+
+import javax.swing.*;
+import java.awt.event.*;
+public class GUI
+{
+   public GUI()
+   {
+     frame();
+       
+    }
+   
+   public void frame()
+   {
+     JFrame f = new JFrame("Moves List");
+     f.setVisible(true);
+     f.setSize(400, 400);
+     f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     
+     JPanel p = new JPanel();
+     JButton b1 = new JButton("Tackle");
+     JButton b2 = new JButton("Heal");
+     JButton b3 = new JButton("Headbutt"); 
+     JButton b4 = new JButton("Growl"); 
+     b1.addActionListener(new ActionListener()
+     {
+       public void actionPerformed(ActionEvent e)
+       {
+          
+          Test.testingMethod();
+        }
+         
+     });
+     b2.addActionListener(new ActionListener()
+     {
+       public void actionPerformed(ActionEvent e)
+       {
+          
+          Test.testingMethod();
+        }
+         
+     });
+     b3.addActionListener(new ActionListener()
+     {
+       public void actionPerformed(ActionEvent e)
+       {
+          
+          Test.testingMethod();
+        }
+         
+     });
+     b4.addActionListener(new ActionListener()
+     {
+       public void actionPerformed(ActionEvent e)
+       {
+          
+          Test.testingMethod();
+        }
+         
+     });
+     p.add(b1);
+     p.add(b2);
+     p.add(b3);
+     p.add(b4);
+     f.add(p);
+   }
+   
+   
+   public static void main(String arg[])
+   {
+     new GUI(); 
+       
+    }
+
+
+
+
+
+
+
+
+
+}
