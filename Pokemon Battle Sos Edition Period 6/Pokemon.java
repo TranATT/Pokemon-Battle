95c3ef25c8c8cbf72fde0bc714e4218cca02f9d1
JeremyLawton <Jeremy.Lawton@comcast.net>
2017-05-10 14:06:34 -0700

    Update Pokemon.java

--- Pokemon Battle Sos Edition Period 6/Pokemon.java
+++ Pokemon Battle Sos Edition Period 6/Pokemon.java
@@ -1,36 +1,38 @@
 
 public class Pokemon implements PokemonInterface {
     private int HP; //the current HP of the Pokemon
-    private ArrayList <Object> attacks; //list of possible attacks and their respective damage
     
-    public Pokemon(String attack1, int atack1, int pp1, String attack2, int atack2, int pp2, String attack3, int atack3, int pp3, String attack4, int atack4, int pp4, int health) {
-        attacks = new ArrayList <Object> ();
-        attacks.add(attack1); //name
-        attacks.add(atack1);  //damage
-        attacks.add(pp1);     //pp
-        attacks.add(attack2); //name
-        attacks.add(atack2);  //damage
-        attacks.add(pp2);     //pp
-        attacks.add(attack3); //name
-        attacks.add(atack3);  //damage
-        attacks.add(pp3);     //pp
-        attacks.add(attack4); //name
-        attacks.add(atack4);  //damage
-        attacks.add(pp4);     //pp
+    public Pokemon() 
+    {
         HP = health;
+    }
+    
+    public int attack1()
+    {
         
     }
-    
-    public int attack() {
-        //return attack number depending on which button is used
+    public int attack2()
+    {
+
     }
+    public int attack3()
+    {
+
+    }
+    public int attack4()
+    {
+
+    }
+    
     
 
     //change the current HP of the Pokemon
-    public void setHP(int health) {
+    public void setHP(int health)
+    {
         HP = health;
     }
-    public int getHP() {
+    public int getHP()
+    {
         return HP;
     }
 }
