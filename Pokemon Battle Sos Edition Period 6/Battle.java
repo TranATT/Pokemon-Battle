2986950386d11519e52f29367260ef24ffd49e00
ShreySh <ss893822@gmail.com>
2017-05-16 13:49:21 -0700

    Update Battle.java

--- Pokemon Battle Sos Edition Period 6/Battle.java
+++ Pokemon Battle Sos Edition Period 6/Battle.java
@@ -1,13 +1,56 @@
-public class Battle {
-    private Pokemon pokemon1;
-    private Pokemon pokemon2;
-    
-    public Battle(String pokemon1, String pokemon2) {
-        if (pokemon1.equals(test) || pokemon2.equals(test)) {
-            if (pokemon1.equals(test))
-                return pokemon1;
-            if (pokemon2.equals(test))
-               return pokemon2;
-        }
+    public class Battle {
+     private Pokemon player1;
+     private Pokemon player2;
+
+     public Battle() {
+      String pokemon1 = choose();
+      String pokemon2 = choose();
+      if (pokemon1.equals("Tigair") || pokemon2.equals("Tigair")) {
+       if (pokemon1.equals("Tigair"))
+        player1 = new Pokemon("Tigair")
+       if (pokemon2.equals("Tigair"))
+        player1 = new Pokemon("Tigair")
+      }
+     }
+
+     public String choose() {
+      JFrame f = new JFrame(Pokemon);
+      f.setVisible(true);
+      f.setSize(400, 400);
+      f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+      JPanel p = new JPanel();
+      //http://www.fantasynamegenerators.com/pokemon-names.php
+      JButton b1 = new JButton("Tigair");
+      JButton b2 = new JButton("Bamboa");
+      JButton b3 = new JButton("Flamiplume");
+      JButton b4 = new JButton("Hyedrill");
+      b1.addActionListener(new ActionListener() {
+       public void actionPerformed(ActionEvent e) {
+        return "Tigair";
+       }
+      });
+      b2.addActionListener(new ActionListener() {
+       public void actionPerformed(ActionEvent e) {
+        return "Bamboa";
+       }
+      });
+      b3.addActionListener(new ActionListener() {
+       public void actionPerformed(ActionEvent e) {
+        return "Flamiplume";
+       }
+
+      });
+      b4.addActionListener(new ActionListener() {
+       public void actionPerformed(ActionEvent e) {
+        return "Hyedrill";
+       }
+      });
+
+      p.add(b1);
+      p.add(b2);
+      p.add(b3);
+      p.add(b4);
+      f.add(p);
+     }
     }
-}
