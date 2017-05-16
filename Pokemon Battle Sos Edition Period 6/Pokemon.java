3d101393161afcd659c5cea2e5abf8a30cfd57fc
ShreySh <ss893822@gmail.com>
2017-05-16 13:09:25 -0700

    Update Pokemon.java
    Added attack details

--- Pokemon Battle Sos Edition Period 6/Pokemon.java
+++ Pokemon Battle Sos Edition Period 6/Pokemon.java
@@ -3,9 +3,22 @@ public class Pokemon implements PokemonInterface {
     private int HP; //the current HP of the Pokemon
     private Random randomizer; //an object to generate random numbers
     
-    public Pokemon() 
-    {
-        HP = 99;
+    public Pokemon(String attack1, int atack1, int pp1, String attack2, int atack2, int pp2, String attack3, int atack3, int pp3, String attack4, int atack4, int pp4, int health) {
+        attacks = new ArrayList <Object> ();
+        attacks.add(attack1); //name
+        attacks.add(atack1);  //damage
+        attacks.add(pp1);     //pp
+        attacks.add(attack2); //name
+        attacks.add(atack2);  //damage
+        attacks.add(pp2);     //pp
+        attacks.add(attack3); //name
+        attacks.add(atack3);  //damage
+        attacks.add(pp3);     //pp         
+        attacks.add(attack4); //name
+        attacks.add(atack4);  //damage
+        attacks.add(pp4);     //pp
+        HP = health;          //health
+
         randomizer = new Random();
     }
     
