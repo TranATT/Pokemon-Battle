f39cddf74320187441485b713df50018ba69d18b
ShreySh <ss893822@gmail.com>
2017-05-16 12:57:53 -0700

    Update Battle.java

--- Pokemon Battle Sos Edition Period 6/Battle.java
+++ Pokemon Battle Sos Edition Period 6/Battle.java
@@ -5,9 +5,9 @@ public class Battle {
     public Battle(String pokemon1, String pokemon2) {
         if (pokemon1.equals(test) || pokemon2.equals(test)) {
             if (pokemon1.equals(test))
-                pokemon1;
+                return pokemon1;
             if (pokemon2.equals(test))
-                pokemon2;
+               return pokemon2;
         }
     }
 }
