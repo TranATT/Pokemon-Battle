19963bd680e4696d810b7b53465ae05f225bd0a4
JeremyLawton <Jeremy.Lawton@comcast.net>
2017-05-19 13:07:50 -0700

    Update Battle.java

--- Pokemon Battle Sos Edition Period 6/Battle.java
+++ Pokemon Battle Sos Edition Period 6/Battle.java
@@ -1,52 +1,67 @@
+import javax.swing.*;
+import java.awt.event.*;
     public class Battle {
      private Pokemon player1;
      private Pokemon player2;
-
+     private String chosenName;
      public Battle() {
-      String pokemon1 = choose();
-      String pokemon2 = choose();
-      if (pokemon1.equals("Tigair") || pokemon2.equals("Tigair")) {
-       if (pokemon1.equals("Tigair"))
-        player1 = new Pokemon("Tigair")
-       if (pokemon2.equals("Tigair"))
-        player1 = new Pokemon("Tigair")
-      }
+      choose();
+      player1  = new Pokemon(chosenName);
+      choose();
+      player2  = new Pokemon(chosenName);
+      
+      System.out.println("Player one is " + player1.getName());
+      System.out.println("Player two is " + player2.getName());
      }
 
-     public String choose() {
-      JFrame f = new JFrame(Pokemon);
+     public void choose() {
+      JFrame f = new JFrame("Pokemon Selection");
       f.setVisible(true);
       f.setSize(400, 400);
       f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
       JPanel p = new JPanel();
+      
       //http://www.fantasynamegenerators.com/pokemon-names.php
-      JButton b1 = new JButton("Tigair");
+      JButton b1 = new JButton("Sos Boss");
       JButton b2 = new JButton("Bamboa");
       JButton b3 = new JButton("Flamiplume");
       JButton b4 = new JButton("Hyedrill");
       b1.addActionListener(new ActionListener() {
        public void actionPerformed(ActionEvent e) {
-        return "Tigair";
+        chosenName = "Sos Boss";
+        System.out.println("Sos Boss has been chosen.");
+        
+        //System.exit(1);
        }
       });
+      
       b2.addActionListener(new ActionListener() {
        public void actionPerformed(ActionEvent e) {
-        return "Bamboa";
+        chosenName = "Bamboa";
+        System.out.println("Bamboa has been chosen.");
+        
+           //     System.exit(1);
        }
       });
+      
       b3.addActionListener(new ActionListener() {
        public void actionPerformed(ActionEvent e) {
-        return "Flamiplume";
+        chosenName = "Flamiplume";
+        System.out.println("Flamiplume has been chosen.");
+        
+        //System.exit(1);
        }
 
       });
+      
       b4.addActionListener(new ActionListener() {
        public void actionPerformed(ActionEvent e) {
-        return "Hyedrill";
+        chosenName = "Hyedrill";
+        System.out.println("Hyedrill has been chosen.");
+        
+           //     System.exit(1);
        }
       });
-
       p.add(b1);
       p.add(b2);
       p.add(b3);
