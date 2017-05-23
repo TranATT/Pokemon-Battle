61425f0d78edc7fa7dd998163057153e719f00bf
ShreySh <ss893822@gmail.com>
2017-05-23 14:27:43 -0700

    Update Pokemon.java

--- Pokemon Battle Sos Edition Period 6/Pokemon.java
+++ Pokemon Battle Sos Edition Period 6/Pokemon.java
@@ -14,44 +14,54 @@ public class Pokemon
         theName = name;
     }
     
-    public void tackle(Pokemon target)
+    public int tackle(Pokemon target, int pp)
     {
-        int damage = 6 + randomizer.nextInt(5);
-        if (growlNextAttack)
-        {
-            damage = damage * 2;
-            growlNextAttack = false;
+        if (pp > 0) {
+            int damage = (int)(6 + (inverseGaussian(1,3) * 5));
+            if (growlNextAttack)
+            {
+                damage = damage * 2;
+                growlNextAttack = false;
+            }
+            target.setHP(target.getHP() - damage);
+            if (target.getHP() <= 0)
+            {
+                System.out.println(target + " is dead.");
+            }
+            else
+            {
+                            System.out.println(target + " got tackled by " + theName + "and took " + damage + " damage and has " + target.getHP() + " health remaining.");
+            }
+            int ppAfter = pp - 1;
+            System.out.println("Tackle can now be used" + ppAfter + "more times");
+            return ppAfter;
         }
-        target.setHP(target.getHP() - damage);
-        if (target.getHP() <= 0)
-        {
-            target.setHP(0);
-            System.out.println(target + " is dead.");
-        }
-        else
-        {
-            System.out.println(target + " took " + damage + " damage and has "
-                               + target.getHP() + " health remaining.");
-        }
+        return 0;
     }
-    public void heal(Pokemon target)
+    public int heal(Pokemon target, int pp)
     {
-        int healing = 5 + randomizer.nextInt(4);
+         if (pp > 0) {
+        int healing = (int)(5 + (inverseGaussian(1,3) * 4));
         target.setHP(target.getHP() + healing);
         if (target.getHP() > 99)
         {
             target.setHP(99);
-            System.out.println(target + " at full health.");
+            System.out.println(target + " is at full health.");
         }
         else
         {
-            System.out.println(target + " healed for " + healing + " HP and now has " 
-                               + target.getHP() + " HP.");
+            System.out.println(target + " get healed by " + theName + " for " + healing + " HP and now has " + target.getHP() + " HP.");
         }
+         int ppAfter = pp - 1;
+            System.out.println("Tackle can now be used" + ppAfter + "more times");
+            return ppAfter;
+        }
+        return 0;
     }
-    public void headbutt(Pokemon target)
+    public int headbutt(Pokemon target, int pp)
     {
-        int damage = 10 + randomizer.nextInt(3);
+         if (pp > 0) {
+        int damage = (int)(10 + (inverseGaussian(1,3) * 3));
         if (growlNextAttack)
         {
             damage = damage * 2;
@@ -65,18 +75,109 @@ public class Pokemon
         }
         else
         {
-            System.out.println(target + " took " + damage + " damage and has "
-                               + target.getHP() + " health remaining.");
+            System.out.println(target + " got headbutted by " + theName + "and took " + damage + " damage and has " + target.getHP() + " health remaining.");
         }
+         int ppAfter = pp - 1;
+            System.out.println("Tackle can now be used" + ppAfter + "more times");
+            return ppAfter;
+        }
+        return 0;
     }
-    public void growl(Pokemon target)
+    public int growl(Pokemon target, int pp)
     {
+         if (pp > 0) {
         growlNextAttack = true;
         System.out.println(target + "'s next attack will deal extra damage.");
+         int ppAfter = pp - 1;
+            System.out.println("Tackle can now be used" + ppAfter + "more times");
+            return ppAfter;
+        }
+        return 0;
+    }
+    public int fireball(Pokemon target, int pp) {
+         if (pp > 0) {
+        int damage = (int)(3 + (inverseGaussian(1,2) * 12));
+        if (growlNextAttack)
+        {
+            damage = damage * 2;
+        }
+        target.setHP(target.getHP() - damage);
+        if (target.getHP() <= 0)
+        {
+            target.setHP(0);
+            System.out.println(target + " is dead.");
+        }
+        else
+        {
+            System.out.println(target + " got burned by " + theName + "and took " + damage + " damage and has " + target.getHP() + " health remaining.");
+        }
+        growlNextAttack = true;
+         int ppAfter = pp - 1;
+            System.out.println("Tackle can now be used" + ppAfter + "more times");
+            return ppAfter;
+        }
+        return 0;
+    }
+    public int punch(Pokemon target, int pp) {
+         if (pp > 0) {
+        int damage = (int)(2 + (inverseGaussian(1,3) * 2));
+        if (growlNextAttack)
+        {
+            damage = damage * 2;
+        }
+        target.setHP(target.getHP() - damage);
+        if (target.getHP() <= 0)
+        {
+            target.setHP(0);
+            System.out.println(target + " is dead.");
+        }
+        else
+        {
+            System.out.println(target + " got punched by " + theName + "and took " + damage + " damage and has " + target.getHP() + " health remaining.");
+        }
+         int ppAfter = pp - 1;
+            System.out.println("Tackle can now be used" + ppAfter + "more times");
+            return ppAfter;
+        }
+        return 0;
+    }
+    public int whip(Pokemon target, int pp) {
+         if (pp > 0) {
+        int damage = (int)(10 + (inverseGaussian(1,4) * 2));
+        if (growlNextAttack)
+        {
+            damage = (int)(damage * 1.3);
+        }
+        target.setHP(target.getHP() - damage);
+        if (target.getHP() <= 0)
+        {
+            target.setHP(0);
+            System.out.println(target + " is dead.");
+        }
+        else
+        {
+            System.out.println(target + " got whipped by " + theName + "and took " + damage + " damage and has " + target.getHP() + " health remaining.");
+        }
+         int ppAfter = pp - 1;
+            System.out.println("Tackle can now be used" + ppAfter + "more times");
+            return ppAfter;
+        }
+        return 0;
     }
     
-    
+    public double inverseGaussian(double mu, double lambda) {
+       //default = 1, 2
+       double v = randomizer.nextGaussian();   // sample from a normal distribution with a mean of 0 and 1 standard deviation
+       double y = v*v;
+       double x = mu + (mu*mu*y)/(2*lambda) - (mu/(2*lambda)) * Math.sqrt(4*mu*lambda*y + mu*mu*y*y);
+       double test = randomizer.nextDouble();  // sample from a uniform distribution between 0 and 1
+       if (test <= (mu)/(mu + x))
+              return x;
+       else
+              return (mu*mu)/x;
+    }
 
+    
     //change the current HP of the Pokemon
     public void setHP(int health)
     {
